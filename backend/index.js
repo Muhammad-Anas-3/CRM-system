bffb13ff3ce87c020f42fc76d590fe6861590bf4
Muhammad Anas <muhammadanas222888@gmail.com>
2024-09-01 16:44:42 +0500

    cors origin url changes

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:5173', // Frontend URL
+    origin: 'https://crm-system-five.vercel.app', // Frontend URL
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
 }));
 
